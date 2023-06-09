2844139af4be40248b2150fe31fa17761cb6e064
Gabriel Nunes <gaseinesdzn@hotmail.com>
2023-06-09 11:39:28 -0300

    animação do menu ajustada

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,20 @@
 function navMenu(){
     const burguer = document.getElementById('burguer')
     const itens = document.getElementById('navid')
+    const menu = document.getElementById('menu')
 
-
-    if(itens.style.display == 'block'){
+    if(itens.style.transform == 'translateY(0%)'){
         burguer.style.boxShadow = 'none'
         burguer.style.borderBottom = 'none'
         burguer.style.background = 'none'
-        itens.style.opacity = 0
-        itens.style.display = 'none'
+        itens.style.transform = 'translateY(-100%)'
+        menu.style.height= '24px'
     }else{
         burguer.style.boxShadow = '1px -5px 5px rgba(0, 0, 0, 0.208)'
         burguer.style.borderBottom = '1px solid black'
         burguer.style.background = '#5D5D5F'
-        itens.style.display = 'block'
-        itens.style.opacity = 1
+        itens.style.transform = 'translateY(0%)'
+        menu.style.height = '264px'
+        
     }
 }
