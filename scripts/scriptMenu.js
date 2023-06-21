04e4e504c615fd2b02dbff3e3a460fc9207249d5
Gabriel Nunes <gaseinesdzn@hotmail.com>
2023-06-20 22:24:19 -0300

    Menu completado

--- scripts/scriptMenu.js
+++ scripts/scriptMenu.js
@@ -67,3 +67,318 @@ function up2(){
         btn.style.display = 'none'
     }
 }
+
+function down3(){
+    let desc = document.getElementById('desc3')
+    let btn = document.getElementById('btn-desc-3')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up3(){
+    let desc = document.getElementById('desc3')
+    let btn = document.getElementById('btn-desc-3')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down4(){
+    let desc = document.getElementById('desc4')
+    let btn = document.getElementById('btn-desc-4')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up4(){
+    let desc = document.getElementById('desc4')
+    let btn = document.getElementById('btn-desc-4')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down5(){
+    let desc = document.getElementById('desc5')
+    let btn = document.getElementById('btn-desc-5')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up5(){
+    let desc = document.getElementById('desc5')
+    let btn = document.getElementById('btn-desc-5')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down6(){
+    let desc = document.getElementById('desc6')
+    let btn = document.getElementById('btn-desc-6')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up6(){
+    let desc = document.getElementById('desc6')
+    let btn = document.getElementById('btn-desc-6')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down7(){
+    let desc = document.getElementById('desc7')
+    let btn = document.getElementById('btn-desc-7')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up7(){
+    let desc = document.getElementById('desc7')
+    let btn = document.getElementById('btn-desc-7')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down8(){
+    let desc = document.getElementById('desc8')
+    let btn = document.getElementById('btn-desc-8')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up8(){
+    let desc = document.getElementById('desc8')
+    let btn = document.getElementById('btn-desc-8')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down9(){
+    let desc = document.getElementById('desc9')
+    let btn = document.getElementById('btn-desc-9')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up9(){
+    let desc = document.getElementById('desc9')
+    let btn = document.getElementById('btn-desc-9')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down10(){
+    let desc = document.getElementById('desc10')
+    let btn = document.getElementById('btn-desc-10')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up10(){
+    let desc = document.getElementById('desc10')
+    let btn = document.getElementById('btn-desc-10')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function down11(){
+    let desc = document.getElementById('desc11')
+    let btn = document.getElementById('btn-desc-11')
+
+    if(btn.style.display == 'none'){
+        desc.style.display = 'none'
+        desc.style.transform = 'translateY(-100%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '80px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
+
+function up11(){
+    let desc = document.getElementById('desc11')
+    let btn = document.getElementById('btn-desc-11')
+
+    if(btn.style.display == 'none'){
+        desc.style.height = '10px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(-1000%)'
+        btn.style.display = 'block'
+    } else{
+        desc.style.display = 'block'
+        desc.style.height = '50px'
+        desc.style.padding = '10px 0px 10px 0px'
+        desc.style.transform = 'translateY(0%)'
+        btn.style.display = 'none'
+    }
+}
